1b2a578fc340a5f12d1fa9a103d674dd109e7f50
kaiquecodes <kaique.machado.099@ufrn.edu.br>
2022-07-26 12:57:35 -0300

    identa

--- src/main/java/residencia/vendas/servico/ServicoVendas.java
+++ src/main/java/residencia/vendas/servico/ServicoVendas.java
@@ -15,32 +15,26 @@ public class ServicoVendas {
 		BigDecimal precoInicial = BigDecimal.ZERO;
 		BigDecimal desconto = BigDecimal.ZERO;
 		BigDecimal precoFinal = BigDecimal.ZERO;
-		BigDecimal acrescimo = BigDecimal.ZERO;
-		BigDecimal valorFinal = BigDecimal.ZERO;
-				
+		BigDecimal valorFinal = BigDecimal.ZERO;				
 						
-		for(Item i : itens) {
-			precoInicial = precoInicial.add(i.preco());
-		}
+		for(Item i : itens) 
+			precoInicial = precoInicial.add(i.preco());		
 				
-		if(precoInicial.doubleValue()>500 && precoInicial.doubleValue()<1000) {
+		if(precoInicial.doubleValue()>500 && precoInicial.doubleValue()<1000) 
 			desconto = precoInicial.multiply(BigDecimal.valueOf(0.1));
-		}
-		else if(precoInicial.doubleValue()>=1000) {
+		
+		else if(precoInicial.doubleValue()>=1000) 
 			desconto = precoInicial.multiply(BigDecimal.valueOf(0.2));
-		}else {
+		
+		else 
 			desconto = BigDecimal.valueOf(0.0);
-		}
-		
-		precoFinal = precoInicial.subtract(desconto);
-		
+				
+		precoFinal = precoInicial.subtract(desconto);		
 				 				
 		BigDecimal valorFrete = calculaFrete(itens);
-		valorFinal = precoFinal.add(valorFrete);
-			
+		valorFinal = precoFinal.add(valorFrete);			
 		
-		return valorFinal;
-		
+		return valorFinal;		
 	}
 	
 	public BigDecimal calculaFrete(List<Item> itens) {
@@ -50,11 +44,9 @@ public class ServicoVendas {
 		BigDecimal acrescimo = BigDecimal.ZERO;
 		BigDecimal desconto = BigDecimal.ZERO;
 			
-		for(Item i : itens) {
-			peso = peso.add(i.peso());
+		for(Item i : itens) 
+			peso = peso.add(i.peso());			
 				
-		}
-		
 		if(peso.doubleValue() < 2.0)
 			frete = peso.multiply(BigDecimal.valueOf(0.0));
 			
@@ -71,8 +63,7 @@ public class ServicoVendas {
 		if(itens.size()>5) 
 			acrescimo = BigDecimal.valueOf(10.0);
 		
-		double valorTotal = frete.doubleValue() + acrescimo.doubleValue() - desconto.doubleValue();
-		
+		double valorTotal = frete.doubleValue() + acrescimo.doubleValue() - desconto.doubleValue();		
 									
 		return BigDecimal.valueOf(valorTotal);
 	}
